2251c8b5f3b85d48346e83d76498d4eafe56e21a
themaleem <zizo9585@gmail.com>
2020-07-18 13:40:37 +0100

    added redux basics to app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
-
+import { createStore} from "redux";
 import Counter from './containers/Counter/Counter';
 import './App.css';
+import reducer from "./store/reducer";
+
+const store=createStore(reducer);
 
 class App extends Component {
   render() {
---
+++ src/store/reducer.js
@@ -0,0 +1,10 @@
+const initialState = {
+  counter: 0
+};
+
+// Reducer
+const reducer = (state = initialState, action) => {
+  return state;
+};
+
+export default reducer;
