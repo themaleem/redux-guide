88107c6f4d71811752dfe5ab1fbb5a2dab979abe
themaleem <zizo9585@gmail.com>
2020-07-20 17:44:35 +0100

    added utility func to update state

--- src/store/reducers/results.js
+++ src/store/reducers/results.js
@@ -1,4 +1,5 @@
 import * as actionTypes from "../actions/actionTypes";
+import { updateObject } from "../utility";
 
 const initialState = {
   results: [],
@@ -8,21 +9,20 @@ const initialState = {
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.STORE_RESULT:
-      return {
-        ...state,
-        results: state.results.concat({ id: new Date(), value: action.counter }),
-      };
+      return updateObject(state, {
+        results: state.results.concat({
+          id: new Date(),
+          value: action.counter,
+        }),
+      });
     case actionTypes.DELETE_RESULT:
       const newResults = state.results.filter(
         (item) => item.id !== action.elemId
       );
-      return {
-        ...state,
-        results: newResults,
-      };
+      return updateObject(state, { results: newResults });
+    default:
+      return state;
   }
-
-  return state;
 };
 
 export default reducer;
